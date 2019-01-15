e09d5ede87ec717508effaa15b5e27e4392e4c37
Arash <arash.mh01@gmail.com>
2019-01-14 17:21:21 -0800

    Add boolean methods to tile

--- tile.rb
+++ tile.rb
@@ -3,6 +3,13 @@ require_relative "board"
 class Tile
     
     def initialize(board)
-        
+        @board = board
     end
+
+    def bombed?(pos)
+        x, y = pos
+        return true if @board[x][y] == 'B'
+        false
+    end
+    
 end
