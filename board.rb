f222f9b588c89f006f03d444c22b55ed826f4dfa
Arash <arash.mh01@gmail.com>
2019-01-14 17:07:45 -0800

    Randomize bombs in board

--- board.rb
+++ board.rb
@@ -4,10 +4,14 @@ class Board
         @board = Array.new(9){Array.new(9)}
     end
 
-    # def randomized_bomb(board)
-    #     10.times do 
-            
-    #     end
-        
-    # end
+    # randomly assign 10 bombs in the board. Mark them with 'B'
+    def randomized_bomb(board)
+        10.times do 
+            row = rand(0..9)
+            column = rand(0..9)
+            @board[row][column] = 'B'
+        end
+    end
+
+    attr_reader :board
 end
