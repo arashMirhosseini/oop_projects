febee8e211f2dc40d8c1fd8a88d9884cad5f8381
Arash <arash.mh01@gmail.com>
2019-01-14 17:36:56 -0800

    Add neighbors method to tile

--- tile.rb
+++ tile.rb
@@ -2,14 +2,41 @@ require_relative "board"
 
 class Tile
     
-    def initialize(board)
+    def initialize(board, position)
         @board = board
+        @position = position 
     end
 
-    def bombed?(pos)
-        x, y = pos
+    def bombed?
+        x, y = @position
         return true if @board[x][y] == 'B'
         false
     end
+
+    def flagged?
+        x, y = @position
+        return true if @board[x][y] == "F"
+        false
+    end
+
+    def reveal?
+        x, y = @position
+        return true if @board[x][y] == '_'
+        false
+    end
+
+    def neighbors
+        i, j = @position
+        neighbors_ar = []
+        x = [-1, -1, -1, 0, 0, 1, 1, 1]
+        y = [-1, 0, 1, -1, 1, -1, 0, 1]
+        x.each_with_index do |num, k|
+            neighbors_ar << Tile.new(board, [i+num, j+y[k]])
+        end
+        neighbors_ar
+    end
+
     
+
+    attr_reader :board, :position
 end
