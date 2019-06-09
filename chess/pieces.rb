57e227236c651c36f7013f63e12a493ef1081308
Arash <arash.mh01@gmail.com>
2019-06-09 12:58:08 -0700

    Update

--- chess/pieces.rb
+++
@@ -1,8 +0,0 @@
-require_relative "piece"
-require_relative "nullPiece"
-require_relative "rook"
-require_relative "knight"
-require_relative "king"
-require_relative "queen"
-require_relative "bishop"
-require_relative "pawn"
